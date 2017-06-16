435a8dbc572a53aede10f7211c118da0a26eb313
Romain Racamier-Lafon <romain.racamier@gmail.com>
2017-06-16 15:35:42 +0200

    bumped

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.onload = function () {
         data: {
             app: {
                 name: 'Musitop',
-                version: '2.2.0'
+                version: '2.2.1'
             },
             isConnected: false,
             isMobile: (typeof window.orientation !== 'undefined'),
